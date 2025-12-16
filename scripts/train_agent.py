Fixed train script and a2c normalization

scripts/train_agent.py
@@ -1,8 +1,8 @@
-# import comet_ml
 import hydra
 import sys
 import torch
 import numpy as np
+import random
 from pathlib import Path
 from tqdm import tqdm
 from hydra.utils import instantiate
@@ -23,6 +23,10 @@ def main(cfg: DictConfig):
 
     train_seed = cfg["training"]["seed"]
     console_logger.info(f"Training Seed : {train_seed}")
+    random.seed(train_seed)
+    torch.manual_seed(train_seed)
+    np.random.seed(train_seed)
+    torch.cuda.manual_seed_all(train_seed)
 
     env = instantiate(cfg["env"])(seed=train_seed)
     env.reset(seed=train_seed)
@@ -53,7 +57,6 @@ def main(cfg: DictConfig):
     console_logger.info(f"Agent: {agent_name}")
 
     nb_env_steps = cfg["training"]["nb_env_steps"]
-    train_batch_size = cfg["training"]["batch_size"]
 
     train_episode_metrics = Metrics()
     train_step_metrics = Metrics()
@@ -80,6 +83,9 @@ def main(cfg: DictConfig):
     metrics_logger.log_code("./src/")
     metrics_logger.log_code("./scripts/")
     metrics_logger.log_code("./configs/")
+    metrics_logger.log_parameters(
+        {"state_size": state_size, "action_size": action_size}
+    )
     verbose_log_ep_interval = cfg["logging"]["verbose_log_ep_interval"]
     step_logging_freq = cfg["logging"]["step_logging_freq"]
     save_model_step_freq = cfg["logging"]["save_model_step_freq"]
@@ -88,37 +94,49 @@ def main(cfg: DictConfig):
     episode_count = 0
     video_start_every = cfg["logging"]["video_start_every"]
     video_frames_per_clip = cfg["logging"]["video_frames_per_clip"]
-    recording = False
-    frames_left = 0
-    clip_start_step = None
+    recording = True
+    frames_left = video_frames_per_clip
+    clip_start_step = 1
+    clip_end_step = video_frames_per_clip
+    next_milestone_end = video_start_every
 
     for env_step in pbar:
-        if (env_step % video_start_every) == 0 and not recording or env_step == 1:
+        start_step = clip_end_step - video_frames_per_clip + 1
+        if (not recording) and (env_step == start_step) and (start_step >= 1):
             recording = True
             frames_left = video_frames_per_clip
             clip_start_step = env_step
 
-        action = agent.action(state)   # returns action only (with cache)
+        action = agent.action(state)
         next_state_dict, reward, terminated, truncated, info = env.step(action)
         next_state = next_state_dict["state"]
         done = terminated or truncated
-        train_logs = agent.on_env_transition(state, action, reward, next_state, done, train_batch_size, env_step) 
+        train_logs = agent.on_env_transition(
+            state, action, reward, next_state, done, env_step
+        )
         state = next_state
 
-
         if recording:
-            recorder.record_step(action=action, reward=reward, done=done)
+            recorder.record_step(action=action, reward=reward, done=done, info=info)
             frames_left -= 1
 
-            if frames_left <= 0:
+            if env_step == clip_end_step or frames_left <= 0:
                 seg_path = (
-                    video_output_dir / f"train_step_{clip_start_step:08d}_{env_step:08d}.mp4"
+                    video_output_dir
+                    / f"train_step_{clip_start_step:08d}_{env_step:08d}.mp4"
                 )
                 recorder.save_segment(str(seg_path))
                 metrics_logger.log_video(str(seg_path), step=env_step)
                 recorder.reset_recording()
                 recording = False
+                frames_left = 0
                 clip_start_step = None
+
+                if clip_end_step == video_frames_per_clip:
+                    clip_end_step = next_milestone_end
+                else:
+                    next_milestone_end += video_start_every
+                    clip_end_step = next_milestone_end
 
         if env_step % step_logging_freq == 0:
             if train_logs.get("train_loss", None) is not None:
@@ -249,7 +267,6 @@ def main(cfg: DictConfig):
         if env_step % save_model_step_freq == 0:
             agent.save(output_weights_folder / Path(f"{agent_name}_{env_step}.pt"))
 
-
         step_metrics = train_step_metrics.data
 
         for metric_name in step_metrics.keys():
@@ -289,6 +306,7 @@ def main(cfg: DictConfig):
         recorder.save_segment(str(seg_path))
         metrics_logger.log_video(str(seg_path), step=env_step)
         recorder.reset_recording()
+
 
 if __name__ == "__main__":
     main()

src/ev_charging/a2c.py
@@ -7,7 +7,9 @@ from ev_charging.stats import RunningMeanStd
 
 
 class ActorNet(nn.Module):
-    def __init__(self, state_size: int, action_size: int, hidden_dim: int, nb_layers: int):
+    def __init__(
+        self, state_size: int, action_size: int, hidden_dim: int, nb_layers: int
+    ):
         super().__init__()
         layers = []
         L = max(1, nb_layers)
@@ -43,15 +45,16 @@ class CriticNet(nn.Module):
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return self.net(x)  # (B, 1)
 
+
 class ActorCriticAgent:
     """
-        A2C update (one-step TD):
-          td_target = r + gamma * V(next_state) * (1 - done)
-          advantage  = td_target - V(state)                 # advantage = td_error
+    A2C update (one-step TD):
+      td_target = r + gamma * V(next_state) * (1 - done)
+      advantage  = td_target - V(state)                 # advantage = td_error
 
-        actor_loss  = -log π(a|s) * td_error.detach()
-        actor_loss ​ = −logπ(a∣s)⋅advantage.detach() - entropy_coef * entropy ---> when we consider entropy term
-        critic_loss = td_error**2 -->  HERE:  critic_loss = Huber( V(s), td_target )
+    actor_loss  = -log π(a|s) * td_error.detach()
+    actor_loss ​ = −logπ(a∣s)⋅advantage.detach() - entropy_coef * entropy ---> when we consider entropy term
+    critic_loss = td_error**2 -->  HERE:  critic_loss = Huber( V(s), td_target )
     """
 
     def __init__(
@@ -66,8 +69,9 @@ class ActorCriticAgent:
         model_hidden_dim: int,
         model_nb_layers: int,
         entropy_coef: float = 0.0,
-        normalize_rewards: bool = False):
-        
+        normalize_rewards: bool = False,
+    ):
+
         self.state_size = state_size
         self.action_size = action_size
         self.seed = seed
@@ -88,8 +92,12 @@ class ActorCriticAgent:
             self.device = torch.device(device)
 
         # Build actor and critic neural networks
-        self.actor = ActorNet(state_size, action_size, model_hidden_dim, model_nb_layers).to(self.device)
-        self.critic = CriticNet(state_size, model_hidden_dim, model_nb_layers).to(self.device)
+        self.actor = ActorNet(
+            state_size, action_size, model_hidden_dim, model_nb_layers
+        ).to(self.device)
+        self.critic = CriticNet(state_size, model_hidden_dim, model_nb_layers).to(
+            self.device
+        )
 
         self.actor_optimizer = optim.Adam(self.actor.parameters(), lr=actor_lr)
         self.critic_optimizer = optim.Adam(self.critic.parameters(), lr=critic_lr)
@@ -114,7 +122,6 @@ class ActorCriticAgent:
         self.actor.eval()
         self.critic.eval()
 
-
     def action(self, state: torch.Tensor):
         """
         Stochastic action for training.
@@ -125,12 +132,12 @@ class ActorCriticAgent:
         if state.dim() == 1:
             state = state.unsqueeze(0)  # (1, state_size)
 
-        logits = self.actor(state)              # (1, action_size)
+        logits = self.actor(state)  # (1, action_size)
         dist = Categorical(logits=logits)
 
         action = dist.sample()  # (1,)
-        log_prob = dist.log_prob(action)                # (1,)
-        entropy = dist.entropy()                        # (1,)
+        log_prob = dist.log_prob(action)  # (1,)
+        entropy = dist.entropy()  # (1,)
 
         # cache for update()
         self._last_action = action
@@ -180,7 +187,9 @@ class ActorCriticAgent:
             r = float(self.reward_rms.normalize(r))
 
         reward_t = torch.tensor([r], dtype=torch.float32, device=self.device)  # (1,)
-        done_t = torch.tensor([float(done)], dtype=torch.float32, device=self.device)  # (1,)
+        done_t = torch.tensor(
+            [float(done)], dtype=torch.float32, device=self.device
+        )  # (1,)
 
         # Critic values
         value = self.critic(state).squeeze(1)  # (1,)
@@ -190,8 +199,8 @@ class ActorCriticAgent:
             td_target = reward_t + (1.0 - done_t) * self.gamma * next_value  # (1,)
 
         advantage = td_target - value  # (1,)
-        advantage = (advantage - advantage.mean()) / (advantage.std() + 1e-8)
-
+        if advantage.numel() > 1:
+            advantage = (advantage - advantage.mean()) / (advantage.std() + 1e-8)
 
         # critic update
         critic_loss = self.critic_criterion(value, td_target)
@@ -244,9 +253,8 @@ class ActorCriticAgent:
         """
         self.actor.load_state_dict(ckpt["actor"])
         self.critic.load_state_dict(ckpt["critic"])
-        
-    def on_env_transition(self, state, action, reward, next_state, done, train_batch_size, env_step) -> dict:
-        return self.update(state, reward, next_state, done)
-    
 
-    
+    def on_env_transition(
+        self, state, action, reward, next_state, done, env_step
+    ) -> dict:
+        return self.update(state, reward, next_state, done)

src/ev_charging/ddqn.py
@@ -22,6 +22,7 @@ class DDQNAgent:
         device: str,
         model_hidden_dim: int,
         model_nb_layers: int,
+        train_batch_size: int,
     ):
         self.eps_start = eps_start
         self.eps_end = eps_end
@@ -40,6 +41,7 @@ class DDQNAgent:
         self.model_hidden_dim = model_hidden_dim
         self.model_nb_layers = model_nb_layers
         self.reward_rms = RunningMeanStd()
+        self.train_batch_size = train_batch_size
 
         if device is None:
             self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -140,14 +142,14 @@ class DDQNAgent:
         action = torch.argmax(q_values[0])
         return action
 
-    def update(self, batch_size, env_step) -> dict:
+    def update(self, env_step) -> dict:
         """
         Perform update using a minibatch from memory.
         """
-        if len(self.memory) < batch_size or env_step < self.learning_starts:
+        if len(self.memory) < self.train_batch_size or env_step < self.learning_starts:
             return {}
         indices = torch.randperm(len(self.memory), generator=self.generator)[
-            :batch_size
+            : self.train_batch_size
         ]
 
         minibatch = [self.memory[i] for i in indices.tolist()]
@@ -215,7 +217,8 @@ class DDQNAgent:
         """
         self.target_model.load_state_dict(self.model.state_dict())
 
-    def on_env_transition(self, state, action, reward, next_state, done, train_batch_size, env_step) -> dict:
-        self.add_to_replay_buffer(state, action, reward, next_state, done)    
-        return self.update(batch_size=train_batch_size, env_step=env_step)
-    
+    def on_env_transition(
+        self, state, action, reward, next_state, done, env_step
+    ) -> dict:
+        self.add_to_replay_buffer(state, action, reward, next_state, done)
+        return self.update(env_step=env_step)